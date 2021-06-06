78a254a6976b47f2306e690486bec6d2c7bc1b3f
Derek Beckford <dbeck1207@icloud.com>
2021-06-06 17:23:42 -0500

    added logic to high score and reset page

--- Assets/logic.js
+++ Assets/logic.js
@@ -1,4 +1,4 @@
-var highScoreEl = document.querySelector("#highscores");
+var finaleEl = document.querySelector("#finale");
 var timerEl = document.querySelector("#timer");
 var questionEl = document.querySelector("#question");
 var openerEl = document.querySelector("#opener");
@@ -10,115 +10,200 @@ var buttonA = document.querySelector("#answerbuttona");
 var buttonB = document.querySelector("#answerbuttonb");
 var buttonC = document.querySelector("#answerbuttonc");
 var buttonD = document.querySelector("#answerbuttond");
+var finalpageEl = document.querySelector("#finalpage");
+var finalScoreEl = document.querySelector("#scorefinal");
+var submitButtonEl = document.querySelector("#submit");
+var initialFieldEl = document.querySelector("#initials")
+var resetButtonEl = document.querySelector("#resetbutton");
+var highscoresEl = document.querySelector("#highscores");
+var highscoreItemEl = document.querySelector("#highscoreitem");
 
+
+var i = 0;
 var score = 0;
 var timeLeft = 60;
 var correct;
+var interval;
 
-var questions = [
-    { 
-        question: 'What does DOM stand for?', 
+var questions = [{
+        question: 'What does DOM stand for?',
         answers: [
-                    'A. Document Object Manipulation', 
-                    'B. Dictation Obstacle Manipulation', 
-                    'C. Document Object Model', 
-                    'D.Document Object Malfunction'
+            'A. Document Object Manipulation',
+            'B. Dictation Obstacle Manipulation',
+            'C. Document Object Model',
+            'D.Document Object Malfunction'
         ],
         correctAnswer: 2,
     },
-    { 
-        question: 'Which of the following is NOT a Javascript datatype?', 
+    {
+        question: 'Which of the following is NOT a Javascript datatype?',
         answers: [
-                    'A. NaN', 
-                    'B. Undefined', 
-                    'C. String', 
-                    'D. Function'
-                ],
+            'A. NaN',
+            'B. Undefined',
+            'C. String',
+            'D. Function'
+        ],
         correctAnswer: 0,
     },
     {
-        question: 'Which is NOT a JavaScript Framework?', 
+        question: 'Which is NOT a JavaScript Framework?',
         answers: [
-                    'A. Python Script', 
-                    'B. Django', 
-                    'C. NodeJS', 
-                    'D. JQuery'
-                ],
+            'A. Python Script',
+            'B. Django',
+            'C. NodeJS',
+            'D. JQuery'
+        ],
         correctAnswer: 1,
     },
-    { 
-        question: 'What does HTML stand for?', 
+    {
+        question: 'What does HTML stand for?',
         answers: [
-                    'A. HyperText Markup Language', 
-                    'B. HyperText Markup Link', 
-                    'C. HyperText Marked Language', 
-                    'D. None of the Above '
-                ],
+            'A. HyperText Markup Language',
+            'B. HyperText Markup Link',
+            'C. HyperText Marked Language',
+            'D. None of the Above '
+        ],
         correctAnswer: 0,
     },
     {
-        question: 'Which of the options is the correct "for loop" syntax? ', 
+        question: 'Which of the options is the correct "for loop" syntax? ',
         answers: [
-                    'A. for ([initialExpression]; [conditionalExpression]) {}',
-                    'B. for ([initialExpression]; [incrementExpression]; [conditional Expression}) {}', 
-                    'C. for ([conditionalExpression]; [initialExpression]; [incrementExpression}) {}', 
-                    'D. for ([initialExpression]; [conditionalExpression]; [incrementExpression}) {}'
+            'A. for ([initialExpression]; [conditionalExpression]) {}',
+            'B. for ([initialExpression]; [incrementExpression]; [conditional Expression}) {}',
+            'C. for ([conditionalExpression]; [initialExpression]; [incrementExpression}) {}',
+            'D. for ([initialExpression]; [conditionalExpression]; [incrementExpression}) {}'
         ],
         correctAnswer: 3,
     }
 ];
-var i = 0;
-var showQuestion = function(i){
+
+var showQuestion = function (i) {
+    if (i < questions.length) {
         questionEl.innerHTML = questions[i].question;
         buttonA.innerHTML = questions[i].answers[0];
         buttonB.innerHTML = questions[i].answers[1];
         buttonC.innerHTML = questions[i].answers[2];
         buttonD.innerHTML = questions[i].answers[3];
+    } else {
+        finaleEl.textContent = "All done!";
+        finalScoreEl.textContent = "Your score is " + score;
+        finalpageEl.style.display = 'initial';
+        clearInterval(interval);
+        divContainerEl.style.display = 'none';
+    }
 }
 
+//checking player answer logic
 
-var checkAnswer = function(choice){
-    if(choice === questions[i].correctAnswer) {
+var checkAnswer = function (choice) {
+    buttonA.disabled = true;
+    buttonB.disabled = true;
+    buttonC.disabled = true;
+    buttonD.disabled = true;
+    if (choice === questions[i].correctAnswer) {
+        //corect anaswer
         score++;
-        checkAnswerEl.innerHTML = 'Correct!';
+        checkanswer.innerHTML = 'Correct!';
+    } else {
+        //wrong answer decrease time by 10 seconds for a wrong answer
+        timeLeft = timeLeft - 10;
+        checkanswer.innerHTML = 'Wrong!';
     }
-    else {
-        checkanswerEl.innerHTML = 'Wrong!';
-    }
+    //add class when to show if the answer is right or wrong 
     document.querySelector("#line").className = "line";
     document.querySelector("#checkanswer").className = "checkanswer";
     i++;
-    setTimeout(function() {
+    setTimeout(function () {
         //remove class
-        document.querySelector("#line").remove("line");
-        document.querySelector("#checkanswer").remove("checkanswer");
+        checkanswer.innerHTML = '';
+        document.querySelector("#line").classList.remove("line");
+        document.querySelector("#checkanswer").classList.remove("checkanswer");
+        buttonA.disabled = false;
+        buttonB.disabled = false;
+        buttonC.disabled = false;
+        buttonD.disabled = false;
         showQuestion(i);
-    
-    }, 1000);
+    }, 700);
 }
 
-
-
-window.onload = function(){
+//hiding the question and final page when the page loads 
+window.onload = function () {
     divContainerEl.style.display = 'none';
+    finalpageEl.style.display = 'none';
+    highscoresEl.style.display = 'none';
 }
 
-startButtonEl.addEventListener("click", function(){
+//showing the question and hiding the initial start page 
+startButtonEl.addEventListener("click", function () {
     openerEl.style.display = 'none';
     divContainerEl.style.display = 'initial';
     startTimer();
     showQuestion(i);
-}
-)
+})
 
-var startTimer = function (){
-    setInterval(function(){ 
+//start timer 
+var startTimer = function () {
+    interval = setInterval(function () {
         if (timeLeft >= 1) {
             timerEl.textContent = 'Timer: ' + timeLeft;
             timeLeft--;
         } else {
-            timerEl.textContent = "Times Up!"
+            // if player runs out of time
+            finaleEl.textContent = "Times up!";
+            finalScoreEl.textContent = "Your score is " + score;
+            divContainerEl.style.display = 'none';
+            finalpageEl.style.display = 'initial';
             clearInterval();
         }
     }, 1000);
 }
+
+//store high score
+
+submitButtonEl.addEventListener("click", function () {
+    if (initialFieldEl.value.length < 2 || initialFieldEl.value.length > 2) {
+        alert("Initials are just two letters! Try again");
+    } else if (initialFieldEl.value === "") {
+        alert("Initials cannot be blank")
+    } else {
+        saveHighScore();
+    }
+})
+
+var saveHighScore = function () {
+    var maxHighscores = 10;
+    var highscores = JSON.parse(localStorage.getItem("highscores")) ?? [];
+    var currentPlayer = initialFieldEl.value.trim().toUpperCase();
+    var newScore = {
+        currentPlayer,
+        score
+    }
+
+    highscores.push(newScore);
+
+    highscores.sort(function (a, b) {
+        return b.score - a.score;
+    });
+
+    highscores.splice(maxHighscores);
+
+    localStorage.setItem("highscores", JSON.stringify(highscores));
+
+    highscoresEl.style.display = 'initial';
+    finalpageEl.style.display = 'none';
+
+
+    for (var i = 0; i < JSON.parse(localStorage.getItem("highscores")).length; i++) {
+        highscoreItemEl.innerHTML += "<li>" + JSON.parse(localStorage.getItem("highscores"))[i].currentPlayer + ': ' + JSON.parse(localStorage.getItem("highscores"))[i].score + "</li>";
+    }
+}
+
+
+resetButtonEl.addEventListener("click", function () {
+    highscoresEl.style.display = "none";
+    openerEl.style.display = "";
+    timerEl.innerHTML = "Timer: Press Start";
+    i = 0;
+    score = 0;
+    timeLeft = 60;
+})
